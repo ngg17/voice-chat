942831901cdaa13bafb28b703a113a043bd2e998
Noyonika Gupta <71960903+ngg17@users.noreply.github.com>
2022-11-13 09:49:22 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -11,6 +11,11 @@ myVideo.muted = true;
 
 let myStream;
 
+navigator.mediaDevices.getUserMedia({audio: true, video: true}).then((stream)=>{
+    mystream = stream
+    addVideoStream(myVideo, stream)
+})
+
 function addVideoStream(video, stream) {
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
